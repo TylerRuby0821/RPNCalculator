fb7a92c34fef0d23f60fde980c207f07ce664d96
tyler <tylerruby0314@yahoo.com>
2021-08-30 15:51:35 -0500

    Corrected output when entering one number at a time. Calculations seem to be working smooth. Need to still address validation of input(letters, symbols, etc)

--- calculator.py
+++ calculator.py
@@ -1,36 +1,46 @@
-import os
-import operator
+from os import system, name           # Importing from os for out clear function
+import operator                       # Importing operator madule for later use
 
-operations = {
+def clear():                          # Declaring a basic clear function to declutter the command line and terminal upon launch
+  if name == 'nt':
+    _= system('cls')
+  else:
+    _= system('clear')
+
+operations = {                        # Defining an operations dictionary with the user of the operator module for later
   "+" : operator.add,
   "-" : operator.sub,
   "/" : operator.truediv,
   "*" : operator.mul
 }
-print("Welcome to my RPN Calculator")     #Basic Welcome message to let the user know what this program is.
+
+#Start by Clearing the terminal, to prevent clutter and make the calculator stand out on its own
+#Print a greeting to the user, as well as a brief description of an RPN Calculator and an example.
+#Send them on their way to calculating!
+
+clear()
+print("Welcome to my RPN Calculator\n")
 print("With an RPN calculator, please enter all values first, followed by the operations.")
+print("Example:\nInput: 8 2 +\nOutput: 10\n")
 print("Enter q to exit the program.")
 print("Happy Calculating!")
-            
-stack = []               # Initializing a Stack
-while True:            #while loop, allowing exit of the calculator when q is the input
-  user_input = input('') #opening up an input for the user and assigning it to our variable
-  if user_input == "q":
-    exit()
-  split = user_input.split(' ')
-  for char in split:
-    print(char)
-    if char == ' ':
-      pass
-    if char in operations:
+
+stack = []                          # Initializing a Stack
+while True:                         # While loop, allowing exit of the calculator when q is the input and keeping our session from closing without user input
+  user_input = input('')            # Opening up an input for the user and assigning it to our variable
+  if user_input == "q":             # If the user inputs "q" the program should close
+    print("Calculations Complete!") # Brief message before closing for more dynamic user interaction
+    exit()                          # Closes the program
+  split = user_input.split(' ')     # Spliting the user input on spaces and assigning it to a variable
+  for char in split:                # Iterate through the split input
+    if char in operations:          # Checking if the current character is an operator, if so we pop the first two items off the stack, save them to
+                                    #     variables and perform the operation using them, before inserting it back into the stack.
       op2 = stack.pop()
-      print("OP 1", op2)
       op = stack.pop()
-      print("OP 2", op)
-      print("stack after pops", stack)
       stack.insert(0, operations[char](int(op), int(op2)))
-      print("Stack after operation", stack)
-    else:
+    else:                           # If the char is not an operator, we simply append it to the stack.
       stack.append(char)
-      print(stack)
-  print(stack[0])
+  if len(stack) == 1:
+    print(stack[0])                # Printing the ouput
+  else:
+    print(stack[len(stack)-1])
