89d7b770f20ef40865d3c99869a84f17201d381d
tyler <tylerruby0314@yahoo.com>
2021-08-30 14:14:45 -0500

    Closer to a working solution but still running into flaws, working on debugging loss of stack

--- calculator.py
+++ calculator.py
@@ -1,3 +1,13 @@
+import os
+import operator
+
+operations = {
+  "+" : operator.add,
+  "-" : operator.sub,
+  "/" : operator.truediv,
+  "*" : operator.mul
+}
+
 class Calculator:
   """
     Setting up a Class for out calculator.
@@ -7,21 +17,30 @@ class Calculator:
     """
     Primary function to run the calculations
     """
-    current = []
-    operands = ["+", "-", "/", "*"]
-    if operands not in s:
-      current.append(s)
-      return s
-    else:
-      new_calculation = s.split(' ')
-      return output
-
+    if s == "q":
+      exit()
+    split = s.split(' ')
+    stack = []
+    for char in split:
+      print(char)
+      if char == ' ':
+        pass
+      if char in operations:
+        op2 = stack.pop()
+        op = stack.pop()
+        stack.append(operations[char](int(op), int(op2)))
+      else:
+        stack.append(char)
+        print(stack)
+    print(stack.pop())
 
 print("Welcome to my RPN Calculator")     #Basic Welcome message to let the user know what this program is.
 print("With an RPN calculator, please enter all values first, followed by the operations.")
+print("Enter q to exit the program.")
+print("Happy Calculating!")
 calculator = Calculator()                     #Creating a new instance of our calculator class
 user_input = ""                               #Creating a variable to hold user input
 while user_input is not ["q"]:                #while loop, allowing exit of the calculator when q is the input
-  user_input = input('')                      #opening up an input for the user and appending it to our variable
+  user_input = input('')                      #opening up an input for the user and assigning it to our variable
   output = calculator.calculate(user_input)   #setting up an output variable, where we runt our calculate function on the users input
   print(output)                               # returning the output to the user
