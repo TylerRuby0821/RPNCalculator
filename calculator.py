5a2b0989a975dd6713e9df38f8bbcbf32ec74499
tylerruby0821 <tylerruby0821@gmail.com>
2021-09-03 10:47:07 -0500

    Cleaned up all the comments, and double checked functionality. Still looking into adding unittest for increased testing when changes are made down the line.

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@ from os import system, name           # Importing from os for out clear function
 import operator                       # Importing operator madule for later use
 import re                             # Imported for Regex Checker
 
-# Declaring a basic clear function to declutter the command line and terminal upon launch
 def clear():
+  # Declaring a basic clear function to declutter the command line and terminal upon launch
   if name == 'nt':
     _= system('cls')
   else:
     _= system('clear')
 
-# Regex character check, to look for letters not used for commands
 def char_check(i):
+  # Regex character check, to look for letters not used for commands
   return re.search(r'[a-zA-Z]$', i)
 
 class RPNCalculator:
@@ -29,8 +29,9 @@ class RPNCalculator:
     self.operations = operations
 
   def handle_command(self, user_in):
-    if user_in == "q":             # If the user inputs "q" the program should close
-        print("Calculations Complete!") # Brief message before closing for more dynamic user interaction
+    # Handles input characters used as commands.
+    if user_in == "q":             
+        print("Calculations Complete!") 
         exit()
     if user_in == "c":
       self.stack = []
@@ -38,23 +39,23 @@ class RPNCalculator:
 
                     
   def calculate(self, user_input):
-    split = user_input.split(' ')     # Spliting the user input on spaces and assigning it to a variable
-    for char in split:                # Iterate through the split input
-      if char in self.operations:          # Checking if the current character is an operator, if so we pop the first two items off the stack, save them to
+    # Main calculation function, where the users input is taken in, split up, and passed to neccesary functions.
+    split = user_input.split(' ')     
+    for char in split:                
+      if char in self.operations:          
         if len(self.stack) < 2:
           print("Error: Not enough values to perform an operation.")
           continue
         self.stack.append(self.operations[char](float(self.stack.pop(-2)), float(self.stack.pop())))
         # print("After Operation",stack)
       else:
-        try:                           # If the char is not an operator, we simply append it to the stack.
+        try:                           
           self.stack.append(float(char))
         except ValueError:
           print("Error: Non-Used Character input, Please enter real numbers, Valid operators(+, - , /, *) or q to exit.")
           continue
     if len(self.stack) >= 1:
-      return self.stack[-1]              # Printing the ouput
-
+      return self.stack[-1]              
 
 
 
@@ -75,7 +76,7 @@ print("Happy Calculating!\n")
 stack = []
 # While loop, allowing exit of the calculator when q is the input and keeping our session from closing without user input
 while True:
-  # Opening up an input for the user and assigning it to our variable
+  # Main loop keeping the cli open for unlimited calculations, until closed by command.
   user_input = input('> ')
   if char_check(user_input):
     new_calc.handle_command(user_input)
--- test_calculator.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import calculator
-
-def test_for_accurate_answer():
-  assert calculator.calculate("8 2 +") == 10
